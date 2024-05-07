aa52ae4c8d67630c9a0a75277219ac5c336f8f58
Ben Do <bdo.appworkshop@gmail.com>
2024-05-07 18:54:02 -0500

    Add project files.

---
+++ AthleisurceAPI/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿namespace AthleisurceAPI.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quanity { get; set; }
+
+        public string Size { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        public string Category { get; set; }
+    }
+}
