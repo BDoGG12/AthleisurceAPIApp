664f23f72bf01a0491602bf9a0cf1d08892d26fa
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 11:02:58 -0500

    Added CustomerModel.

---
+++ AthleisurceAPI/Models/CustomerModel.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLibrary.Models;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AthleisurceAPI.Models
+{
+    public class CustomerModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public ShippingAddressModel Address { get; set; } = new ShippingAddressModel();
+        public List<ProductModel> Products { get; set; } = new List<ProductModel>();
+    }
+}
