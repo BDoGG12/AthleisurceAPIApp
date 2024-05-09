ddbe61fe0d6b028ec8d689d922380d52ab65af52
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 11:05:18 -0500

    Added ShippingAddressModel.

---
+++ AthleisurceAPI/Models/ShippingAddressModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AthleisurceAPI.Models
+{
+    public class ShippingAddressModel
+    {
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
