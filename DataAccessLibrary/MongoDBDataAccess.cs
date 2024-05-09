bce4695d2d7a831196005e699bd0c37c5519662b
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 11:07:12 -0500

    Added MongoDBDataAccess.

---
+++ DataAccessLibrary/MongoDBDataAccess.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace DataAccessLibrary
+{
+    public class MongoDBDataAccess
+    {
+
+        private IMongoDatabase db;
+
+        public MongoDBDataAccess(string dbName, string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            db = client.GetDatabase(dbName);
+        }
+
+        public void InsertRecord<T>(string tableName, T record)
+        {
+            var collection = db.GetCollection<T>(tableName);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string tableName)
+        {
+            var collection = db.GetCollection<T>(tableName);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById<T>(string tableName, int id)
+        {
+            var collection = db.GetCollection<T>(tableName);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.Find(filter).First();
+        }
+
+        public void UpsertRecord<T>(string tableName, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(tableName);
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", id),
+                record,
+                new UpdateOptions { IsUpsert = true });
+        }
+
+        public void DeleteRecord<T>(string tableName, Guid id)
+        {
+            var collection = db.GetCollection<T>(tableName);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+        }
+    }
+}
