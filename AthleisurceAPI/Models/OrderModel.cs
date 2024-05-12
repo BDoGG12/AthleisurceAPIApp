8140dee269a804af34796e8c21cfa91c95441d56
Ben Do <bdo.appworkshop@gmail.com>
2024-05-11 19:19:09 -0500

    Implemented OrderModel.

---
+++ AthleisurceAPI/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLibrary.Models;
+
+namespace AthleisurceAPI.Models
+{
+    public class OrderModel
+    {
+        public Guid Id { get; set; }
+        public List<ProductModel> Items { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
