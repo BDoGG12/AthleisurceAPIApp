e8ba87a79759237d4a3a816f9814bc6b8958b26b
Ben Do <bdo.appworkshop@gmail.com>
2024-05-13 11:15:35 -0500

    Added Subtotal property.

--- AthleisurceAPI/Models/OrderModel.cs
+++ AthleisurceAPI/Models/OrderModel.cs
@@ -5,7 +5,9 @@ namespace AthleisurceAPI.Models
     public class OrderModel
     {
         public Guid Id { get; set; }
-        public List<ProductModel> Items { get; set; }
+        public List<ProductModel> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public CustomerModel Customer { get; set; }
         public DateTime OrderDate { get; set; }
     }
 }
