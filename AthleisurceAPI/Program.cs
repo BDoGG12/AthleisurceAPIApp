9cb15e26c6d486f81db834142f1082189f671f76
Ben Do <bdo.appworkshop@gmail.com>
2024-05-13 11:28:05 -0500

    Added OrderServie to scope.

--- AthleisurceAPI/Program.cs
+++ AthleisurceAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<CartService>(_ => new CartService("MongoAthleisurceDB", "Athleisurce_Cart"));
+builder.Services.AddScoped<CartService>(_ => new CartService("MongoAthleisurceDB", "Athleisurce_Order"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
