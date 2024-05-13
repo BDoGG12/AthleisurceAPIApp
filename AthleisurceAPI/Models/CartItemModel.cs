69099c1370b95321e21c53aceaf574292c42af34
Ben Do <bdo.appworkshop@gmail.com>
2024-05-13 11:13:54 -0500

    Implemented CartItemModel.

---
+++ AthleisurceAPI/Models/CartItemModel.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+
+namespace AthleisurceAPI.Models
+{
+    public class CartItemModel
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public CustomerModel CustomerInfo { get; set; }
+        public ProductModel ProductInfo { get; set; }
+    }
+}
